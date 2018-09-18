ae74572a992e9d5acfc25308985c3fd0e65241bd
Daniel Schoch <d.schoch90@gmail.com>
2018-09-18 20:35:24 +0000

    added puts line breaks

--- lib/epl_table/cli.rb
+++ lib/epl_table/cli.rb
@@ -4,6 +4,7 @@ class EplTable::CLI
 
   def call
     puts "This is the current EPL table:"
+    puts ""
     display_table
     further_info
     goodbye
@@ -11,6 +12,7 @@ class EplTable::CLI
 
   def display_table
     puts <<-DOC.gsub(/^\s*/, "")
+
       1. Chelsea - 5 Wins, 3 Draws, 2 Losses - 18 pts
       2. Spurs - 4 Wins, 2 Draws, 3 Losses - 14 pts
     DOC
@@ -19,11 +21,14 @@ class EplTable::CLI
   def further_info
     input = nil
     while input != "exit"
+      puts ""
       puts "Enter a team's table ranking for more detailed information or type exit:"
       input = gets.strip.downcase
       if input.to_i.between?(1,20)
+        puts ""
         team_details(input)
       elsif input != "exit"
+        puts ""
         puts "Please enter a valid ranking or type exit:"
       end
     end
@@ -31,11 +36,12 @@ class EplTable::CLI
 
 
   def team_details(ranking)
-    EplTable::Team.test
+    EplTable::Team.team_test
     #team = Team.find_team_by_rank(ranking)
   end
 
   def goodbye
+    puts ""
     puts "Check back soon for updated rankings!"
   end
 
