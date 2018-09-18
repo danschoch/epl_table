e92d848a07e73e03850d5d2b603187d376fb18f1
Daniel Schoch <d.schoch90@gmail.com>
2018-09-18 01:21:10 +0000

    display_table method in CLI class

--- lib/epl_table/cli.rb
+++ lib/epl_table/cli.rb
@@ -3,7 +3,15 @@
 class EplTable::CLI
 
   def call
-    puts "this is the EPL table"
+    puts "This is the current EPL table:"
+    display_table
+  end
+
+  def display_table
+    puts <<-DOC
+      1. Chelsea - 5 Wins, 3 Draws, 2 Losses - 18 pts
+      2. Spurs - 4 Wins, 2 Draws, 3 Losses - 14 pts
+    DOC
   end
 
 end
