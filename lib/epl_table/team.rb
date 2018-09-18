3969ed2e27374ed30612e95d3509ee9da7099c91
Daniel Schoch <d.schoch90@gmail.com>
2018-09-18 20:20:50 +0000

    added test Team instance and checked CLI interface

--- lib/epl_table/team.rb
+++ lib/epl_table/team.rb
@@ -1,15 +1,21 @@
+#should matches (prev and next) be separate objects?
+#make sure to reference team instances for prev_opponent and next_opponent
+
 class EplTable::Team
 
   attr_accessor :name, :stadium, :location, :wins, :draws, :losses, :table_points,
   :ranking, :prev_opponent, :next_opponent
 
+  @@all = []
+
   def self.test
+    team = self.new("Chelsea", "Stamford Bridge", "London", "5", "3", "2", "18", "1", "Manchester City", "Burnley")
     puts <<-DOC.gsub(/^\s*/, "")
-      Chelsea - Stamford Bridge, London
-      No. 1 in English Premier League
-      5 Wins, 3 Draws, 2 Losses - 18 pts
-      Previous Fixture : 2-3 Loss v. Manchester City
-      Next Fixture: Burnley (2-1-4)
+      #{team.name} - #{team.stadium} - #{team.location}
+      No. #{team.ranking} in English Premier League
+      #{team.wins} Wins, #{team.draws} Draws, #{team.losses} Losses - #{team.table_points} Points
+      Previous Fixture: 2-3 Loss v. #{team.prev_opponent}
+      Next Fixture: #{team.next_opponent} (2-1-4)
     DOC
   end
 
@@ -24,9 +30,14 @@ class EplTable::Team
     @ranking = ranking
     @prev_opponent = prev_opponent
     @next_opponent = next_opponent
+    @@all << self
   end
 
   #Class Methods
+
+  def self.all
+    @@all
+  end
 
   def self.find_team_by_rank(ranking)
     Team.all.find do |team|
@@ -38,9 +49,3 @@ class EplTable::Team
 
 
 end
-
-
-
-
-
-#should matches (prev and next) be separate objects?
