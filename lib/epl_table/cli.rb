fb5f3cb92a588ad7e25fd575aaf09c196b13b15c
Daniel Schoch <d.schoch90@gmail.com>
2018-09-18 17:36:21 +0000

    add further_info

--- lib/epl_table/cli.rb
+++ lib/epl_table/cli.rb
@@ -5,6 +5,8 @@ class EplTable::CLI
   def call
     puts "This is the current EPL table:"
     display_table
+    further_info
+    team_details
   end
 
   def display_table
@@ -12,6 +14,14 @@ class EplTable::CLI
       1. Chelsea - 5 Wins, 3 Draws, 2 Losses - 18 pts
       2. Spurs - 4 Wins, 2 Draws, 3 Losses - 14 pts
     DOC
+  end
+
+  def further_info
+    puts "Enter a team's table ranking for more detailed information"
+    input = gets.strip
+    Team.all.find do |team|
+      input == team.ranking
+    end
   end
 
 end
