3b50c478f9b790f3e2d9b78f67c1aecce816b866
Daniel Schoch <d.schoch90@gmail.com>
2018-09-19 02:50:15 +0000

    scraper can identify targeted table

--- lib/epl_table/scraper.rb
+++ lib/epl_table/scraper.rb
@@ -4,5 +4,8 @@ require 'open-uri'
 class EplTable::Scraper
   def scrape_table
     doc = Nokogiri::HTML(open("https://www.premierleague.com/tables"))
+    table = doc.css('table').css('tbody.tableBodyContainer').css('tr')
+    team_name = table.css('td.team').css('.long').first.text
+    team_name
   end
 end
