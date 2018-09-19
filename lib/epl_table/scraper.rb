ff8e5608cc6a2d5b65654588ce914034b049327f
Daniel Schoch <d.schoch90@gmail.com>
2018-09-19 02:57:34 +0000

    refined table_row selectors

--- lib/epl_table/scraper.rb
+++ lib/epl_table/scraper.rb
@@ -4,8 +4,9 @@ require 'open-uri'
 class EplTable::Scraper
   def scrape_table
     doc = Nokogiri::HTML(open("https://www.premierleague.com/tables"))
-    table = doc.css('table').css('tbody.tableBodyContainer').css('tr')
-    team_name = table.css('td.team').css('.long').first.text
+    table_row = doc.css('table').css('tbody.tableBodyContainer').css('tr[data-compseason="210"]')
+    team_name = table_row.css('td.team').css('.long').first.text
+
     team_name
   end
 end
